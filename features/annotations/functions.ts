52fe63a803365294d2eb53b04a595da5af4ccc5a
builtbyjosh <pc.refuge@gmail.com>
2021-08-12 22:48:49 -0500

    finished up annotating functions

--- features/annotations/functions.ts
+++ features/annotations/functions.ts
@@ -1,3 +1,39 @@
 const add = (a: number, b: number): number => {
   return a + b
 }
+
+const subtract = (a: number, b: number): number => {
+  return a - b
+};
+
+function divide(a: number, b: number): number {
+  return a / b
+}
+
+const multiply = function(a: number, b: number):number {
+  return a * b
+}
+
+const logger = (msg: string): void => {
+  console.log(msg)
+}
+
+const throwError = (message: string): string => {
+  if(!message){
+    throw new Error(message);
+  }
+  return message
+}
+
+const forecast = {
+  date: new Date(),
+  weather: 'sunny'
+};
+
+const logWeather = ({date, weather}: {date: Date, weather: string}): void => {
+  console.log(date)
+  console.log(weather)
+}
+
+logWeather(forecast);
+
